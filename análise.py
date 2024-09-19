72c50f1c39b7ee9432a0f8b5a0893f823dd148ef
Wellsrs <132627424+Wellsrs@users.noreply.github.com>
2024-09-19 11:54:35 -0300

    Add files via upload

--- análise.py
+++ análise.py
@@ -4,25 +4,17 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
-
-
 # Carregar o arquivo CSV para inspeção
-file_path = r"C:\Users\hanie\Downloads\estudo.csv"
-
+file_path = r"C:\Users\hanie\Downloads\sales.csv"
 df = pd.read_csv(file_path)
 
 print(df)
 # Exibir as primeiras linhas para entender os dados
 df.head(), df.columns, df.info()
 
-
-#--------ANÁLISE EXPLORATÓRIA DE DADOS--------
-
-# Carregar os dados
-df = pd.read_csv('C:/Users/hanie/Downloads/estudo.csv')
+# --------ANÁLISE EXPLORATÓRIA DE DADOS--------
 
 # Visualizar as primeiras linhas
 print(df.head())
@@ -34,13 +26,7 @@ print(df.describe())
 sns.histplot(df['product'], bins=30)
 plt.show()
 
-# Matriz de correlação
-plt.figure(figsize=(10, 8))
-#sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
-plt.show()
-
-
-#--------MODELAGEM E PREVISÃO--------
+# --------MODELAGEM E PREVISÃO--------
 
 # Separar as variáveis independentes e dependentes
 X = df[['quantity']]  
@@ -60,49 +46,25 @@ y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 print(f'Mean Squared Error: {mse}')
 
-
-
-#--------SEGMENTAÇÃO E AGRUPAMENTO--------
+# --------SEGMENTAÇÃO E AGRUPAMENTO--------
 
 # Normalizar os dados (opcional, dependendo dos dados)
 scaler = StandardScaler()
 # Selecione as colunas que você deseja normalizar
-X = df[['price_y', 'price_x', 'quantity']]
+X = df[['price_y', 'quantity', 'price_z' ]]
 X_scaled = scaler.fit_transform(X)
 
-# Aplicar K-Means
-kmeans = KMeans(n_clusters=3)  # Ajuste o número de clusters conforme necessário
-kmeans.fit(X_scaled)
+# Alterar o nome das colunas
+df = df.rename(columns={
+    "name": "Nome", 
+    "sale_id": "id_venda", 
+    "product_id": "id_produto", 
+    "product": "produto", 
+    "price_y": "preço_y", 
+    "quantity": "quantidade",
+    "price_z": "valor_total", 
+    "sale_date": "data_venda"
+})
 
-# Adicionar os rótulos de cluster aos dados
-df['Cluster'] = kmeans.labels_
-
-# Criar os gráficos de dispersão
-fig, axes = plt.subplots(1, 3, figsize=(18, 6))
-
-# Combinação 1: 'price_y' vs 'quantity'
-sns.scatterplot(x='price_y', y='quantity', hue='Cluster', data=df, ax=axes[0])
-axes[0].set_title('price_y vs quantity')
-
-# Combinação 2: 'price_x' vs 'quantity'
-sns.scatterplot(x='price_x', y='quantity', hue='Cluster', data=df, ax=axes[1])
-axes[1].set_title('price_x vs quantity')
-
-# Combinação 3: 'price_y' vs 'price_x'
-sns.scatterplot(x='price_y', y='price_x', hue='Cluster', data=df, ax=axes[2])
-axes[2].set_title('price_y vs price_x')
-
-# Ajustar layout e mostrar os gráficos
-plt.tight_layout()
-plt.show()
-
-#Altera o nome das colunas 
-
-df = df.rename(columns={"name":"Nome", "sale_id":"id_venda", "product_id":"id_produto", "product":"produto", "price_y":"preço_y", "quantity":"quantidade",
-                   "price_x":"preço_x", "created_at":"data_criação", "updated_at":"data_atualização"})
-
-#Caso o Power BI não formatasse a data deveria realizar o processo a seguir:
-#df['data_criação'] = pd.to_datetime(df['data_criação'])
-#df['data_criação_formatada'] = df['data_criação'].dt.strftime('%d-%m-%Y %H:%M')
-
-df.to_csv('dados_com_clusters.csv', index=False)
+# Salvar o DataFrame renomeado em um novo arquivo CSV
+df.to_csv('correctsales.csv', index=False)
